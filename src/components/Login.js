9004b7a0840333f96e3600d0ff46a2ecc59f8d40
smishra11 <msubhasish11@gmail.com>
2020-12-23 18:45:02 +0530

    Created login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,14 +6,35 @@ export default class Login extends Component {
   };
   render() {
     return (
-      <div>
-        <button
-          onClick={this.btnClick}
-          type="button"
-          className="btn btn-primary"
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <div
+          className="card bg-light"
+          style={{ width: '24rem', height: '14rem' }}
         >
-          Log In
-        </button>
+          <div className="card-body">
+            <h3
+              className="card-title text-info"
+              style={{
+                textAlign: 'center',
+              }}
+            >
+              Github oAuth Flow
+            </h3>
+            <p className="card-text">
+              Log in with Github to see the account details and repositories .
+            </p>
+            <div style={{ textAlign: 'center' }}>
+              <button className="btn btn-info">Log In</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
