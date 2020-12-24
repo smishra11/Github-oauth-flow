a4603bd77cf8f79d123588438966a5560762897b
smishra11 <msubhasish11@gmail.com>
2020-12-24 19:45:40 +0530

    showing own data for testing

--- src/components/Myprofile.js
+++ src/components/Myprofile.js
@@ -1,48 +1,74 @@
-import React from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import React, { Component } from 'react';
+import Location from '../assets/location.svg';
+import RepoIcon from '../assets/repo.svg';
 
-const GET_CURRENT_USER = gql`
-  {
-    viewer {
-      login
-      name
-      repositories(first: 10) {
-        edges {
-          node {
-            name
-            id
-          }
-        }
-      }
-    }
+class Myprofile extends Component {
+  componentDidMount() {
+    console.log(this.props.data);
   }
-`;
-
-const Myprofile = () => (
-  <Query query={GET_CURRENT_USER}>
-    {({ loading, error, data }) => {
-      if (loading) return <h4>Loading</h4>;
-      if (error) console.log(error);
-
-      const {
-        viewer,
-        viewer: {
-          repositories: { edges },
-        },
-      } = data;
-      return (
-        <div>
-          {viewer.name} {viewer.login}
-          <ul>
-            {edges.map((edge) => {
-              return <li key={edge.node.id}>{edge.node.name}</li>;
-            })}
-          </ul>
+  render() {
+    const { viewer } = this.props.data;
+    return (
+      <div className="row">
+        <div className="col-4">
+          <img
+            className="profileAvatar"
+            src={this.props.data.viewer.avatarUrl}
+            alt="Avatar"
+          />
+          <div className="avatarText">
+            <h3>{viewer.name}</h3>
+            <h5 className="text-secondary">{viewer.login}</h5>
+            <div
+              style={{ borderTop: '1px solid lightgrey', width: '250px' }}
+            ></div>
+            <p className="text-secondary mt-2" style={{ display: 'flex' }}>
+              <img className="mr-1" src={Location} alt="location" />
+              {viewer.location}
+            </p>
+          </div>
         </div>
-      );
-    }}
-  </Query>
-);
+        <div className="col-8">
+          <div className="row">
+            {viewer.repositories.edges.map((repo) => {
+              return (
+                <div className="col-6" key={repo.node.id}>
+                  <div
+                    className="card bg-light mb-3"
+                    style={{ maxWidth: '25rem' }}
+                  >
+                    <div className="card-body">
+                      <h6
+                        style={{ fontWeight: 'bold', color: 'rgb(3 102 214)' }}
+                        className="card-title d-flex"
+                      >
+                        <img className="mr-2" src={RepoIcon} alt="repo" />{' '}
+                        {repo.node.name}
+                      </h6>
+                      <hr />
+                      <p className="card-text mb-2 ">
+                        Created At - {repo.node.createdAt}
+                      </p>
+                      <p className="card-text">
+                        Github Link -
+                        <a
+                          href={repo.node.url}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          {repo.node.url}
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Myprofile;
