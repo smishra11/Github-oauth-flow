80e9fbf338271fbab3d47f1975168e7e360bf714
smishra11 <msubhasish11@gmail.com>
2020-12-25 19:32:31 +0530

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Gitlogo from '../assets/git.svg';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div style={{ marginBottom: '3rem' }}>
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand navGit" href="#/">
+            <img src={Gitlogo} width="30" height="30" alt="Nav Img" />
+            <span style={{ fontWeight: 'bold', fontSize: '24px' }}>
+              Git OAuth
+            </span>
+          </a>
+          <button className="btn btn-outline-info mr-4">Log Out</button>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
