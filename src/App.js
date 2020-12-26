130a47c63ba0eb1ea8a234de5122fae2eff442d8
smishra11 <msubhasish11@gmail.com>
2020-12-26 20:09:57 +0530

    testing purpose

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import { ApolloProvider } from 'react-apollo';
-import Myprofile from './components/Myprofile';
+import ProfileData from './components/ProfileData';
 import Client from './client';
 import Login from './components/Login';
+import Navbar from './components/Navbar';
+import ProjectDetails from './components/ProjectDetails';
 
 const App = () => {
   return (
     <ApolloProvider client={Client}>
-      <div className="App">
-        <h1>GitHub API</h1>
-        <Myprofile />
-        <Login />
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Login />
+            {/* <Redirect to="/login" /> */}
+          </Route>
+          <Route path="/:username">
+            <Navbar />
+            <ProfileData />
+          </Route>
+          <Route path="/:username/:repo">
+            <Navbar />
+            <ProjectDetails />
+          </Route>
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 };
